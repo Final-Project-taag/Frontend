7943f9f6cb8d961dda6f7aa6e01397d90daece51
ahmad3ud <ahmadabokasemQgmail.com>
2023-03-30 11:57:16 +0200

    frontend boilerplate augefsetz
    es Würde eingefügt:
    LOGO
    login/register  Componentes
    useQuere / useAuthStore Hooks
    grundlage style im bearbeitung

---
+++ src/js/App.jsx
@@ -0,0 +1,54 @@
+import '../scss/App.scss';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './components/Layout';
+import ProtectedPage from './components/ProtectedPage';
+import PrivateRoute from './services/privateRout';
+import Register from './components/Register';
+import Login from './components/Login';
+import useAuthStore from './hooks/useAuthStore';
+
+
+
+
+import { UserCircleIcon } from "@heroicons/react/24/outline";
+
+
+
+
+function App() {
+  const authStore = useAuthStore();
+
+
+  return (
+    <div className="App w-full h-full ">
+      <div className='container w-full h-full '>
+
+        
+        
+
+        
+     
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Layout />}>
+              
+              <Route index element={<h2>zwar musst du dich  jeder refresh anmelden, aber es funktioniert! hoffentlich...</h2>}/>
+
+              <Route path='/register' element={<Register/>} />
+              <Route path='/login' element={<Login/>} />
+
+              <Route element={<PrivateRoute />}>
+                <Route path='/protected' element={<ProtectedPage />} />
+              </Route>
+            </Route>
+            
+          </Routes>
+        </BrowserRouter>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
