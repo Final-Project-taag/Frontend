dc7de9d541eddbbc53f0e85d5ae34a5ca0cbae76
Tenaw-Worku <tenaw@outlook.com>
2023-04-02 18:49:52 +0200

    first commit

--- src/js/components/Layout.jsx
+++ src/js/components/Layout.jsx
@@ -2,7 +2,7 @@
 import useAuthStore from '../hooks/useAuthStore';
 import Header from './Heder';
 import Footer from './Footer';
-
+import AboutUs from './AboutUs';
 
 /* import { UserCircleIcon } from "@heroicons/react/24/outline"; */
 
@@ -12,7 +12,9 @@ function Layout() {
     return (
         <>
             <Header />
+            {/* <AboutUs />  */}
             <Footer />
+          
         
         </>
     );
